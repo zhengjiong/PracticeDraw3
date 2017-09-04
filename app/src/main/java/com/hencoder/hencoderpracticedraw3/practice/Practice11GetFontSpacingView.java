bd7a703ce364a16084cc7f05e53a35d6dffd1697
zhengjiong <zhengjiong2006@gmail.com>
2017-09-04 13:49:10 +0800

    feat<Practice>finish 11 getFontSpacing

--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice11GetFontSpacingView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice11GetFontSpacingView.java
@@ -32,12 +32,23 @@ public class Practice11GetFontSpacingView extends View {
         super.onDraw(canvas);
 
         // 使用 Paint.getFontSpacing() 来获取推荐的行距
+        /**
+         * 获取推荐的行距。
+
+         即推荐的两行文字的 baseline 的距离。这个值是系统根据文字的字体和字号自动计算的。
+         它的作用是当你要手动绘制多行文字（而不是使用 StaticLayout）的时候，
+         可以在换行的时候给 y 坐标加上这个值来下移文字。
+
+         */
         float spacing = 20;
 
         canvas.drawText(text, 50, 100, paint);
 
+        spacing = paint.getFontSpacing();
         canvas.drawText(text, 50, 100 + spacing, paint);
 
-        canvas.drawText(text, 50, 100 + spacing * 2, paint);
+
+        spacing += paint.getFontSpacing();
+        canvas.drawText(text, 50, 100 + spacing, paint);
     }
 }
