1e7c8f8bc662cd1711349e6c6693a7f769d3c970
zhengjiong <zhengjiong2006@gmail.com>
2017-09-04 13:37:59 +0800

    feat<Practice>finish 09 setTextScaleX

--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice09SetTextScaleXView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice09SetTextScaleXView.java
@@ -27,6 +27,8 @@ public class Practice09SetTextScaleXView extends View {
         paint.setTextSize(60);
 
         // 使用 Paint.setTextScaleX() 来改变文字宽度
+        //设置文字横向放缩。也就是文字变胖变瘦。
+        paint.setTextScaleX(1.5f);
     }
 
     @Override
